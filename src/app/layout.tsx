52c6ada9100a459baa68ee0711c43303c7c7bf3d
Vincent Thomas <beguine-badgast-0p@icloud.com>
2024-05-01 13:47:11 +0200

    ci: creating gh actions pipeline

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,18 +4,30 @@ import { cn } from "@/utils/cn";
 import { CookiesBanner } from "@/components/cookies-banner";
 import { cookies } from "next/headers";
 import { LoginButton } from "@/components/login-button";
+import type { Metadata } from "next";
+
+const APP_NAME = "Next.js Example application";
+const APP_DESCRIPTION =
+  "Next.js example application is a template for building powerfull applications";
+
+// Default
+export const metadata: Metadata = {
+  title: APP_NAME,
+  description: APP_DESCRIPTION,
+  openGraph: {
+    type: "website",
+    title: APP_NAME,
+    description: APP_DESCRIPTION
+  }
+};
+
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en">
       <body className={cn(globalStyles)}>
         {cookies().get("yes-cookies")?.value !== "1" && <CookiesBanner />}
         <LoginButton />
-        <img
-          height="50"
-          width="50"
-          src={`${env.appUrl}/assets/user-profile`}
-          alt="testing"
-        />
+        <img height="50" width="50" src="/assets/user-profile" alt="testing" />
         {children}
       </body>
     </html>
